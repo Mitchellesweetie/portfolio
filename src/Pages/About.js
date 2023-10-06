8dff5fdf39b785d8e0c1442c089c03a5982b77bc
Mitchellesweetie <mitchellengoli@gmail.com>
2023-10-06 16:18:51 +0300

    first form

---
+++ src/Pages/About.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+      <p>
+        
+<h1>Mitchelle Ngoli Mwachoo</h1>
+I am a skilled web developer based in Kenya, specializing in web development, 
+web design, and mobile application development.
+ I bring a versatile and adaptable approach to my work.
+      </p>
+    </div>
+  )
+}
+
+export default About
